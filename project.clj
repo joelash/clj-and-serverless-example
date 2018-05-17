80ad86d4eb4b6ee963e29d77989166a5b86455d6
Joel Friedman <asher.friedman@gmail.com>
2018-05-17 08:37:47 -0500

    Removing dependencies on aws libraries and clojurify objects

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject hello-world "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [com.amazonaws/aws-lambda-java-core "1.2.0"]
-                 [com.amazonaws/aws-lambda-java-events "2.1.0"]]
+                 [camel-snake-kebab "0.4.0"]]
   ;; :java-source-paths ["src/java"]
   :aot :all)
--- src/hello_joel.clj
+++ src/hello_joel.clj
@@ -1,33 +1,39 @@
 (ns hello-joel
   (:require
+    [camel-snake-kebab.core :as csk]
+    [camel-snake-kebab.extras :as csk.extras]
     [clojure.data.json :as json]
     [clojure.java.io :as io]
     [clojure.pprint :as pp]
-    [clojure.string :as string])
-  (:import
-    [com.amazonaws.services.lambda.runtime.events APIGatewayProxyResponseEvent]))
+    [clojure.string :as string]))
 
 ;; Adapted from https://github.com/uswitch/lambada
 (defmacro defapigateway
   "Create a named class that can be invoked as a AWS Lambda function."
   [name args & body]
+  (assert (= 1 (count args)) "Invalid arity")
   (let [this-ns (ns-name *ns*)
         class-name (symbol (str this-ns "." name))
-        fn-name (symbol (str "-" name))]
+        fn-name (symbol (str "-" name))
+        event-name (symbol (first args))]
     `(do
        (gen-class
         :name ~class-name
-        :methods [^:static [~name [java.util.Map] com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent]])
-       (defn ~fn-name
-         ~args
-         (let [resp# (do ~@body)]
-           ;; TODO test setHeaders and how do i do a when or cond->
-           (doto (APIGatewayProxyResponseEvent.)
-             (.setStatusCode (int (:status resp#)))
-             (.setBody (json/write-str (:body resp#)))
-             #_(.setHeaders (json/write-str (:headers resp#)))))))))
+        :methods [^:static [~name [java.util.Map] java.util.Map]])
+       (defn ~fn-name [~event-name]
+         (let [~event-name (->> ~event-name (into {}) (csk.extras/transform-keys csk/->kebab-case-keyword))
+               resp# (do ~@body)]
+           (assert (:status resp#) "Must return a status")
+           (cond-> {"statusCode" (int (:status resp#))}
+             (:body resp#) (assoc "body" (json/write-str (:body resp#)))
+             (:headers resp#) (assoc "headers"
+                                     (->> resp#
+                                          :headers
+                                          (csk.extras/transform-keys csk/->HTTP-Header-Case-String)))
+             :always (java.util.HashMap.)))))))
 
 (defapigateway foo [event]
   (pp/pprint event)
   {:status 200
-   :body {:hello :world3}})
+   :body {:hello :world-joel-3}
+   :headers {:x-foo-bar "125"}})
